e166ba70f43f4c44438ca2e2cdad51a94f5ec89e
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2024-10-19 16:10:23 +0300

    new parser phone book

--- HW4/phone_pook_pot/main.py
+++ HW4/phone_pook_pot/main.py
@@ -1,29 +1,28 @@
-from contacts import add_contact, change_contact, show_phone, show_all
-from parser import parse_input
+from parser import parse_input  
+from contacts import add_contact, change_contact, show_phone, show_all  
 
-def main():
-    print("Welcome to the assistant bot!")  # Вітання
-
-    while True:
+def main():  
+    print("Welcome to the assistant bot!")  # Додаємо вітання  
+    
+    while True:  
         command_input = input("Enter a command: ")  # Отримуємо команду від користувача  
-        command, args = parse_input(command_input)  # Використання функції parse_input() типу деструкткризація
+        cmd, args = parse_input(command_input)  # Парсимо команду та аргументи  
         
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args))
-        elif command == "change":
-            print(change_contact(args))
-        elif command == "phone":
-            print(show_phone(args))
-        elif command == "all":
-            print(show_all())
-        else:
-            print("Invalid command.")
+        if cmd in ["close", "exit"]:  
+            print("Good bye!")  
+            break  
+        elif cmd == "hello":  
+            print("How can I help you?")  
+        elif cmd == "add":  
+            print(add_contact(args))  
+        elif cmd == "change":  
+            print(change_contact(args))  
+        elif cmd == "phone":  
+            print(show_phone(args))  
+        elif cmd == "all":  
+            print(show_all())  
+        else:  
+            print("Invalid command.")  
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     main()
-
--- HW4/phone_pook_pot/parser.py
+++ HW4/phone_pook_pot/parser.py
@@ -1,10 +1,4 @@
-
-
-def parse_input(command_input):  
-    args = command_input.strip().split()  # Розбиваємо ввід на частини  
-    
-    if not args:  
-        return None, []  # Якщо немає аргументів, повертаємо None  
-    
-    # Повертаємо перший аргумент як команду та решту як аргументи  
-    return args[0].strip().lower(), args[1:] 
+def parse_input(user_input):  
+    cmd, *args = user_input.split()  
+    cmd = cmd.strip().lower()  
+    return cmd, args
